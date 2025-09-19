1fb9ba460e9ca440f97fe5165dee695f6d287ecd
alvaro.vizuete <alvaro.vizuete@106-31.info2>
2025-09-19 13:13:01 +0200

    Exercise 5

--- src/Exercise5/exercise5.java
+++ src/Exercise5/exercise5.java
@@ -4,7 +4,7 @@ public class exercise5 {
 
 	public static void main(String[] args) {
 		System.out.println("Name: Álvaro Vizuete Valle");
-		System.out.println("Address: alvaro.vizuete@iesnervion.es");
+		System.out.println("Address: Calle Romulo");
 		System.out.println("Telephone Number: +34 693 23 37 80");
 	}
 
